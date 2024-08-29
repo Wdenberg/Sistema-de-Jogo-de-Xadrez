5e5b69f597d233025369d6698b393d17db9ec213
Wdenberg <wdenbergskillshd@hotmail.com>
2024-08-28 22:32:46 -0300

    Creating position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+    private Integer rows;
+    private Integer columns;
+
+    public Position(Integer rows, Integer columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Integer columns) {
+        this.columns = columns;
+    }
+
+
+    @Override
+    public String toString() {
+        return rows + ", " + columns;
+    }
+}
