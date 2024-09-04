ba421662a3139746e8f2cac206c47f8ced6c1d2a
Wdenberg <wdenbergskillshd@hotmail.com>
2024-09-04 15:09:29 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -33,6 +33,13 @@ public class ChessMatch {
         Piece capturePiece = makeMove(source, target);
         return (ChessPiece) capturePiece;
     }
+
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     private Piece makeMove(Position source, Position target){
         Piece p = board.removePiece(source);
         Piece capturePiece = board.removePiece(target);
