96167e37fa794c46fa76d1d05088a0e61517c1f6
Wdenberg <wdenbergskillshd@hotmail.com>
2024-09-11 20:04:49 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces(){
@@ -34,6 +46,7 @@ public class ChessMatch {
         return (ChessPiece) capturePiece;
     }
 
+
     public boolean[][] possibleMoves(ChessPosition sourcePosition){
         Position position = sourcePosition.toPosition();
         validateSourcePosition(position);
@@ -44,12 +57,20 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturePiece = board.removePiece(target);
         board.placePiece(p, target);
+        nextTurn();
         return capturePiece;
     }
 
+    public void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
     private void  validateSourcePosition(Position position){
         if(! board.thereIsApiece(position)){
             throw new ChessException("There is no piece on source position");
+        }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("the chosen piece is not yours");
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is possible for the chosen piece");
